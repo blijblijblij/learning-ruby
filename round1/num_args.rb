dad4d101613ea40a8bc6e2e54815553719ea5c4b
Rogier Wessel <rwessel@reelmetrics.com>
2016-07-04 06:46:53 +0200

    Method with variable args using *args

--- round1/num_args.rb
+++ round1/num_args.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# one or more method arguments
+
 def num_args (*args)
   length = args.size
   label = length == 1 ? " argument " : " arguments "
@@ -9,4 +11,5 @@ end
 
 puts num_args
 puts num_args(1)
-puts num_args(100, 2.5, "three " * 23)
+puts num_args(100, 2.5, "three " * 3)
+puts num_args(10012, 21235, "twelve " * 23)
