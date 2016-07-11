5780b2f8a30d0eb409066c8d0597ffe879bc0608
Rogier Wessel <rwessel@reelmetrics.com>
2016-07-11 16:56:42 +0200

    Folder restructure

--- Learning ruby, oreilly/4/number_conversions.rb
+++ Learning ruby, oreilly/4/number_conversions.rb
@@ -5,8 +5,10 @@ p Integer("256") # convert a string => 256
 p Integer("0b11110010") # convert a binary number from a string => 242
 p Integer(0177) # convert an octal number => 127
 p Integer(0x20) # convert a hexadecimal number => 32
+# no longer functional, old api
 # p Integer(?z) # convert a character code => 122
 
 p Float(167) # convert an integer => 167.0
 p Float("77") # convert a string => 77.0
+# no longer functional, old api
 # p Float(?a) # convert a character code => 97.0
