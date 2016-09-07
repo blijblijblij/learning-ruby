9f5b8f7dbe677de20819502690ee1ff14ffb0d91
Rogier Wessel <rwessel@reelmetrics.com>
2016-09-07 07:59:44 +0200

    Updated

---
+++ Learning ruby, oreilly/10, More Fun with Ruby/date.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'date'
+
+puts "Date functions"
+
+date = Date.new( 2006, 11, 8 )
+puts date.to_s
+
+today = Date.today
+puts today.to_s
+
+puts "The date was #{date.month}/#{date.day}/#{date.year}."
+puts "The date is #{today.month}/#{today.day}/#{today.year}."
+puts "The next date was #{date.next.to_s} and is #{today.next.to_s}"
