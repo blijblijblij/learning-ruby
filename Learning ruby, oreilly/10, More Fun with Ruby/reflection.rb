901b44274482e74c7d6b9a7e3d4c2e880621854a
Rogier Wessel <rwessel@reelmetrics.com>
2016-09-08 21:45:19 +0200

    Added reflection.rb

---
+++ Learning ruby, oreilly/10, More Fun with Ruby/reflection.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+asiponder = <<whitman
+As I ponder'd in silence,
+Returning upon my poems, considering, lingering long,
+A Phantom arose before me, with distrustful aspect,
+Terrible in beauty, age, and power,
+The genius of poets of old lands
+whitman # => "As I ponder'd in silence, \nReturning upon my poems, considering, lingering long, \nA Phantom arose before me, with distrustful aspect, \nTerrible in beauty, age, and power, \nThe genius of poets of old lands\n"
+
+puts (asiponder.class).to_s
