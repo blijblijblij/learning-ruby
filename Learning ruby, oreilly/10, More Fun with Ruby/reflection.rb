46aae9f74d70b4749bf0825dafc3ebbad8d2f9d2
Rogier Wessel <rwessel@reelmetrics.com>
2016-09-08 21:49:36 +0200

    Updated

--- Learning ruby, oreilly/10, More Fun with Ruby/reflection.rb
+++ Learning ruby, oreilly/10, More Fun with Ruby/reflection.rb
@@ -6,6 +6,12 @@ Returning upon my poems, considering, lingering long,
 A Phantom arose before me, with distrustful aspect,
 Terrible in beauty, age, and power,
 The genius of poets of old lands
-whitman # => "As I ponder'd in silence, \nReturning upon my poems, considering, lingering long, \nA Phantom arose before me, with distrustful aspect, \nTerrible in beauty, age, and power, \nThe genius of poets of old lands\n"
+whitman
 
-puts (asiponder.class).to_s
+puts asiponder.class.to_s
+
+if asiponder.instance_of?(String)
+    asiponder.split.each do |t|
+        puts t
+    end
+end
