211b60dc746d3b4be5019aff977f6d47b9fd3a75
Rogier Wessel <rwessel@reelmetrics.com>
2016-09-08 21:35:54 +0200

    Updated date.rb

--- Learning ruby, oreilly/10, More Fun with Ruby/date.rb
+++ Learning ruby, oreilly/10, More Fun with Ruby/date.rb
@@ -13,3 +13,5 @@ puts today.to_s
 puts "The date was #{date.month}/#{date.day}/#{date.year}."
 puts "The date is #{today.month}/#{today.day}/#{today.year}."
 puts "The next date was #{date.next.to_s} and is #{today.next.to_s}"
+
+date.upto( date + 5 ) { |date| puts date }
