215aa0dd4444bb84491a8dfdd397d92640baf43c
Rogier Wessel <rwessel@reelmetrics.com>
2016-09-08 21:20:33 +0200

    Added Time

--- Learning ruby, oreilly/10, More Fun with Ruby/time.rb
+++ Learning ruby, oreilly/10, More Fun with Ruby/time.rb
@@ -2,19 +2,19 @@
 
 start = Time.now
 
-puts "The day of the week is " + (start.wday).to_s
-puts "The day of the month is " + (start.day).to_s
-puts "The day of the year is " + (start.yday).to_s
+puts 'The day of the week is ' + start.wday.to_s
+puts 'The day of the month is ' + start.day.to_s
+puts 'The day of the year is ' + start.yday.to_s
 bloeb = start.wday * start.day * start.yday
-puts "Mutiplied with eachother, that makes " + (bloeb).to_s
-puts "The year is " + (start.year).to_s
-puts "The hour " + (start.hour).to_s
-puts "The timezone " + (start.zone).to_s
-puts "The minute is " + (start.min).to_s
-puts "The second is " + (start.sec).to_s
+puts 'Mutiplied with eachother, that makes ' + bloeb.to_s
+puts 'The year is ' + start.year.to_s
+puts 'The hour ' + start.hour.to_s
+puts 'The timezone ' + start.zone.to_s
+puts 'The minute is ' + start.min.to_s
+puts 'The second is ' + start.sec.to_s
 
 stop = Time.now
 
 stop + (60 * 60 * 3)
 
-puts "Stop - start is: " + (stop - start).to_s
+puts 'Stop - start is: ' + (stop - start).to_s
