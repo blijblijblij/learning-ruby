d41db56042e25c0af06dbdaf60be8f40d8c09378
Rogier Wessel <rwessel@reelmetrics.com>
2016-09-09 14:52:55 +0200

    Updated

--- Learning Rails 5/1, Starting Up Ruby on Rails/hello01/app/controllers/hello_controller.rb
+++ Learning Rails 5/1, Starting Up Ruby on Rails/hello01/app/controllers/hello_controller.rb
@@ -3,5 +3,6 @@ class HelloController < ApplicationController
     @message="Hello, bloeb v2 .sh!"
     @count=3
     @bonus="This message came from the controller."
+    @bloeb="Bloeb v2 .sh\n" * 200 * 200 + "<h2>.bat</h2>"
   end
 end
