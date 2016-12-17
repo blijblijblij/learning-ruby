ee80c2709ea8171bfbf6e3f5ebd57a66b0dfe8b9
Rogier Wessel <rhwessel@xs4all.nl>
2016-12-17 14:31:20 +0100

    Wip

--- The well grounded Rubyist/Chapter 4/bicycle.rb
+++ The well grounded Rubyist/Chapter 4/bicycle.rb
@@ -8,6 +8,10 @@ class Bicycle
   end
 end
 # tandem class extends on bicycle but has two seats
+# super can be called in 3 manners:
+# - super           : sends all arguments along with the called
+# - super()         : sends no arguments
+# - super (a,b,c)   : sends only the specified list a,b,c
 class Tandem < Bicycle
   def initialize(gears)
     super
