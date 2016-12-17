97175bc932c8df7b7193903917ca640d7b3c4d1a
Rogier Wessel <rhwessel@xs4all.nl>
2016-12-17 14:02:58 +0100

    Update

--- Learning Rails 5/1, Starting Up Ruby on Rails/hello01/app/controllers/hello_controller.rb
+++ Learning Rails 5/1, Starting Up Ruby on Rails/hello01/app/controllers/hello_controller.rb
@@ -1,12 +1,13 @@
 require 'date'
 
+# blalblalblalbla
 class HelloController < ApplicationController
   def index
-    @message="Hello, bloeb v2 .sh!"
-    @count=3
-    @bonus="This message came from the controller."
-    @bloeb="Bloeb v2 .sh\n" * 2 + "<h2>.bat</h2>"
+    @message = 'Hello, bloeb v2 .sh!'
+    @count = 5
+    @bonus = 'This message came from the controller.'
+    @bloeb = "Bloeb v2 .sh\n" * 2 + '<h2>.bat</h2>'
     today = Date.today
-    @today="Today's date is #{today.month}/#{today.day}/#{today.year}."
+    @today = "Today's date is #{today.month}/#{today.day}/#{today.year}."
   end
 end
